934f3166f4fb3b92ad1c54818967b9d738322535
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-11-12 18:28:47 +0300

    m_10_3.py
    для стабильной работы добавил второй lock_main

--- m_10_3.py
+++ m_10_3.py
@@ -1,6 +1,8 @@
 import threading
 import time
 from random import randint
+lock_main = threading.Lock()
+
 
 class Bank(threading.Thread):
     lock = threading.Lock()
@@ -10,9 +12,11 @@ class Bank(threading.Thread):
 
     def deposit(self) -> None:
         for i in range(100):
+            lock_main.acquire()
             num = randint(50, 500)
             self.balance += num
             print(f'deposit {i+1} \nпополнение: {num} текущий таланс: {self.balance}')
+            lock_main.release()
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
             time.sleep(0.3)
@@ -20,6 +24,7 @@ class Bank(threading.Thread):
     def take(self) -> None:
         time.sleep(0.1)
         for j in range(100):
+            lock_main.acquire()
             num2 = randint(50, 500)
             print(f'take {j+1}: запрос {num2} ')
             if num2 <= self.balance:
@@ -27,18 +32,22 @@ class Bank(threading.Thread):
                 print(f'снятие: {num2}, текущий баланс:{self.balance}\n')
             else:
                 print('запрос отклонен\n')
-            self.lock.acquire()
+                self.lock.acquire()
+            lock_main.release()
+
+
             time.sleep(0.3)
 
 
 
 bank1 = Bank()
+
 th1 = threading.Thread(target=Bank.deposit, args=(bank1,))
 th2 = threading.Thread(target=Bank.take, args=(bank1,), daemon=True)
+
 th1.start()
 th2.start()
 th1.join()
-# th2.join()
+th2.join()
 
 print(f'Итоговый баланс: {bank1.balance}')
-print('end')
